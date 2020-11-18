93350581059bd55d2210fdc3cb0eac575efb210e
sandienunez <github email address>
2020-11-17 23:58:09 -0500

    Remove file that was not needed for project, remove notes in code

--- homehero-backend/config/routes.rb
+++ homehero-backend/config/routes.rb
@@ -1,25 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-#   get '/listings'  => 'listings#index' 
-#   #get '/listings', to: 'listings#index'
-# #resources:listings, only: :index 
-
-#  post '/listings'  => 'listings#create' 
-#   get '/listings/:id'  => 'listings#show'
-#   #get '/listings/:id', to: 'listings#show'
-#   get '/listings:id/edit'  => 'listings#edit' 
-#   get '/listings/:id'  => 'listings#update' 
-#   delete '/listings/:id', to: 'listings#destroy'
-
-
-
-
   resources :listings
   
- 
-  # get '/businesses'  => 'businesses#' 
-  # get '/homeservices'  => 'homeservices#' 
 
 end
 
--- homehero-frontend/src/src/service.js
+++
@@ -1,8 +0,0 @@
-class HomeService {
-    constructor(types){
-        this.types = types;
-        HomeService.all.push(this);
-    }
-}
-HomeService.all = [];
-
