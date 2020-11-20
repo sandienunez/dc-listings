09e1fd3ffd9f2dafe812e52451dfec23c5378fe3
sandienunez <github email address>
2020-11-20 14:16:21 -0500

    Update routes to include only option for action methods used in app

--- homehero-backend/config/routes.rb
+++ homehero-backend/config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :listings #do only
+#   get '/listings', to: 'listings#index'
+
+#  post '/listings'  => 'listings#create' 
+#   #get '/listings/:id', to: 'listings#show' 
+#   delete '/listings/:id', to: 'listings#destroy'
+
+
+
+  resources :listings, only: [:index, :show, :create, :destroy]
+
   
 
 end
